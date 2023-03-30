97ea618ef7f43198eace5e6d69632181fc69ba8a
Osmar Neto <88497805+OsmarBaia@users.noreply.github.com>
2023-03-29 23:19:50 -0300

    FrontPage

--- resources/js/page2.js
+++
@@ -1,9 +0,0 @@
-const log = document.getElementById("log");
-
-function logSubmit() {
-  //Get Values
-  let userName = window.localStorage.getItem("nome");
-  log.textContent = `Saved Name: ${userName}`;
-}
-
-logSubmit();
