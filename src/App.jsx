8033b9ed224fe219b591cf64512f4234c4f3c9c5
Osmar Neto <88497805+OsmarBaia@users.noreply.github.com>
2024-05-08 16:12:40 -0300

    Fixed - NavBar
    Fixed navbar scrolldown error, where it would not collapse after loading.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 // React - Bootstrap
+import 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'react-bootstrap'
+
 
 import NavBar from './components/navbar';
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,8 @@ import { useState, useEffect } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-
 import Button from 'react-bootstrap/Button';
+
 
 export default function NavBar() {
     const [activeLink, setActiveLink] = useState('home');
@@ -27,32 +27,35 @@ export default function NavBar() {
     const onUpdateActiveLink = (value) => { setActiveLink(value); }
 
     return (
-        <Navbar className={scrolled ? "scrolled" : ""} expand="lg">
+        <Navbar className={scrolled ? "scrolled" : ""} expand="lg" >
             <Container>
                 <Navbar.Brand href="#home">osmarneto.dev</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav id='navbar-sections' className="me-auto">
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Início</Nav.Link>
-                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Habilidades</Nav.Link>
-                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projetos</Nav.Link>
+                    <Nav className="">
+                        <div id='navbar-sections'>
+                            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Início</Nav.Link>
+                            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Habilidades</Nav.Link>
+                            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projetos</Nav.Link>
+                        </div>
+                        <div id='navbar-socials'>
+                            <Nav.Link className='navbar-link social-media' href="#instagram" target='blank'>
+                                <i class="bi bi-instagram"></i>
+                            </Nav.Link>
+
+                            <Nav.Link className='navbar-link social-media' href="#linkedin" target='blank'>
+                                <i class="bi bi-linkedin"></i>
+                            </Nav.Link>
+
+                            <Nav.Link className='navbar-link social-media' href="#behance" target='blank'>
+                                <i class="bi bi-behance"></i>
+                            </Nav.Link>
+                        </div>
+                        <Button variant="light"><Nav.Link href="#contact" className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('contact')}>
+                            Contato
+                        </Nav.Link></Button>
+
                     </Nav>
-                    <div id='navbar-socials'>
-                        <Nav.Link className='navbar-link social-media' href="#instagram" target='blank'>
-                            <i class="bi bi-instagram"></i>
-                        </Nav.Link>
-
-                        <Nav.Link className='navbar-link social-media' href="#linkedin" target='blank'>
-                            <i class="bi bi-linkedin"></i>
-                        </Nav.Link>
-
-                        <Nav.Link className='navbar-link social-media' href="#behance" target='blank'>
-                            <i class="bi bi-behance"></i>
-                        </Nav.Link>
-                    </div>
-                    <Button variant="light"><Nav.Link href="#contact" className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('contact')}>
-                        Contato
-                    </Nav.Link></Button>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
