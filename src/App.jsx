5e9d3422a7d7b974bfb9bf13829b35cd3006c34d
Osmar Neto <88497805+OsmarBaia@users.noreply.github.com>
2024-05-09 01:47:26 -0300

    Style: Add Theme Saving to LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,21 @@ import 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-
 import NavBar from './components/navbar';
 
 export default function App() {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(() => {
+    return JSON.parse(localStorage.getItem('isDark'));
+  });
 
   return (
     <div className='App' data-theme={isDark ? 'dark' : 'light'}>
       <header className='container-fluid'>
-        <NavBar isDark={isDark} handleChange={() => setIsDark(!isDark)} ></NavBar>
+        <NavBar isDark={isDark} handleChange={
+          function changeTheme() {
+            localStorage.setItem('isDark', JSON.stringify(!isDark));
+            setIsDark(!isDark);
+          }}></NavBar>
       </header>
       <main>
 
