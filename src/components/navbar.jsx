4e1586065ff6837f78d1951e666067d50d21da3a
Osmar Neto <88497805+OsmarBaia@users.noreply.github.com>
2024-05-10 23:29:33 -0300

    Style: Add a navbar collapse work around

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Nav, Navbar, Container } from 'react-bootstrap';
 import { Toggle } from './toggle';
 import './navbar.css'
+
 
 export default function NavBar({ handleChange, isDark }) {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
     const [isHidden, setHidden] = useState(true);
+    const onUpdateActiveLink = (value) => { setActiveLink(value); }
 
     useEffect(() => {
         const onScroll = () => {
@@ -23,50 +25,51 @@ export default function NavBar({ handleChange, isDark }) {
         return () => window.removeEventListener('scroll', onScroll);
     }, [])
 
-    const onUpdateActiveLink = (value) => setActiveLink(value);
 
-    // Navbar Collapse on Link Click
-    const navbarMenu = document.querySelector('#basic-navbar-nav');
-    function onLinkClick() {
-        navbarMenu.classList.toggle('show');
+    const ToggleScreenBlur = () => {
+        navbarScreenBlur.current.classList.toggle('hidden');
     }
-    const navlinks = document.querySelectorAll('a.nav-link');
-    navlinks.forEach((e) => {
-        e.addEventListener('click', onLinkClick);
-    })
 
+    //NavBar Collapse WorkAround
+    const navbarToggler = useRef(null);
+    const navbarNavContainer = useRef(null);
+    const navbarScreenBlur = useRef(null);
+
+    function CollapseNavbar() {
+        navbarNavContainer.current.classList.toggle('show');
+        navbarToggler.current.classList.toggle('collapsed');
+        ToggleScreenBlur;
+    }
 
     return (
-
         <Navbar className={scrolled ? "scrolled" : ""} expand="lg" variant={isDark ? 'dark' : 'light'}>
             <Container>
                 <Navbar.Brand href="#home" className="">OsmarNeto.dev</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setHidden(!isHidden)} />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Toggle ref={navbarToggler} aria-controls="#basic-navbar-nav" onClick={() => { ToggleScreenBlur }} />
+                <Navbar.Collapse ref={navbarNavContainer} id="basic-navbar-nav">
                     <Nav>
-                        <div id='navbar-blur' className={isHidden ? 'hidden' : ''}></div>
+                        <div id='navbar-blur' ref={navbarScreenBlur}></div>
                         <div id='navbar-sections'>
-                            <Nav.Link href="#home" className={activeLink === 'home' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => { onUpdateActiveLink('home') }}>
+                            <Nav.Link href="#home" className={activeLink === 'home' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => { CollapseNavbar(); onUpdateActiveLink('home'); }}>
                                 Início
                             </Nav.Link>
-                            <Nav.Link href="#about" className={activeLink === 'about' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>
+                            <Nav.Link href="#about" className={activeLink === 'about' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => { CollapseNavbar(); onUpdateActiveLink('about'); }}>
                                 Sobre
                             </Nav.Link>
-                            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>
+                            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => { CollapseNavbar(); onUpdateActiveLink('skills'); }}>
                                 Habilidades
                             </Nav.Link>
-                            <Nav.Link href="#jobs" className={activeLink === 'jobs' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('jobs')}>
+                            <Nav.Link href="#jobs" className={activeLink === 'jobs' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => { CollapseNavbar(); onUpdateActiveLink('jobs'); }}>
                                 Serviços
                             </Nav.Link>
-                            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>
+                            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => { CollapseNavbar(); onUpdateActiveLink('projects'); }}>
                                 Projetos
                             </Nav.Link>
-                            <Nav.Link href="#contact" className={activeLink === 'contact' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('contact')}>
+                            <Nav.Link href="#contact" className={activeLink === 'contact' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => { CollapseNavbar(); onUpdateActiveLink('contact'); }}>
                                 Contato
                             </Nav.Link>
                         </div>
-                        <Toggle isChecked={isDark} handleChange={handleChange}
-                        />
+                        <Toggle isChecked={isDark} handleChange={handleChange} />
                     </Nav>
                 </Navbar.Collapse>
             </Container>
