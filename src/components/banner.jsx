fd077a1ac8e61074fbf8d7bc32128ca581990eab
Osmar Neto <88497805+OsmarBaia@users.noreply.github.com>
2024-05-10 21:53:29 -0300

    Feature: Added Banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,125 +1,87 @@
 import React from 'react';
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import TrackVisibility from 'react-on-screen';
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/header-img.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
+import { TypeAnimation } from 'react-type-animation';
 
 //Styles
 import './banner.css';
 
-const ComponentToTrack = ({ isVisible }) => {
-    const style = {
-        background: isVisible ? 'red' : 'blue'
-    };
-    return <span style={style} className="tagline">Welcome to my Portfolio</span>
-}
+// const ComponentToTrack = ({ isVisible }) => {
+//     const style = {
+//         background: isVisible ? 'red' : 'blue'
+//     };
+//     return <span style={style} className="tagline">Welcome to my Portfolio</span>
+// }
+
+const JobList = () => {
+    return (
+        <TypeAnimation
+            sequence={[
+                'Web Designer',
+                1000, // Waits 1s
+                'Desenvolvedor Web',
+                2000, // Waits 2s
+                'Analista de Sistemas',
+                // () => {
+                //     console.log('Sequence completed');
+                // },
+            ]}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+            style={{ fontSize: '2em', display: 'inline-block' }}
+        />
+    );
+};
 
 export default function Banner() {
-
     return (
         <>
             <section className="banner" id="home">
                 <Container>
-                    <Row className="aligh-items-center">
-                        <Col xs={12} md={6} xl={7}>
-                            <TrackVisibility>
-                                {({ isVisible }) => {
-                                    // const style = {
-                                    //     background: isVisible ? 'red' : 'blue'
-                                    // };
-                                    // return <span style={style} className="tagline">Welcome to my Portfolio</span>
-
-                                    // <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                                    //     <span className="tagline">Welcome to my Portfolio</span>
-                                    //     <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                                    //     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                                    //     <button onClick={() => console.log('connect')}>Lets Connect<ArrowRightCircle size={25} /></button>
-                                    // </div>
-                                }}
-                            </TrackVisibility>
-                        </Col>
+                    <Row className="banner-content aligh-items-center flex-md-row-reverse">
                         <Col xs={12} md={6} xl={5}>
-                            <TrackVisibility>
-                                <profilePic />
-                                {({ isVisible }) =>
-                                    <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                                        <img src={headerImg} alt="Header Img" />
-                                    </div>}
-                            </TrackVisibility>
+                            <div className='banner-section banner-pic'>
+                                <img src={headerImg} alt="Profile Picture" />
+                            </div>
+                            {/* <TrackVisibility>
+                                {
+                                    ({ isVisible }) => {
+                                        <div style={'height: 50%'} className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                                            <img src={headerImg} alt="Profile Picture" />
+                                        </div>
+                                    }
+                                }
+                            </TrackVisibility> */}
+                        </Col>
+                        <Col xs={12} md={6} xl={7}>
+                            <div className='banner-section banner-text'>
+                                <h1>Oi! Eu sou <span>Osmar</span></h1>
+                                <h2><TypeAnimation
+                                    sequence={['Analista de Sistemas', 500, 'Web Designer', 500, 'Desenvolvedor Web', 500]}
+
+                                    repeat={Infinity}
+                                /></h2>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam et dolorem fuga accusamus quos delectus doloribus nulla velit, sequi sed, facere laboriosam? Tempore minus ab quaerat consequatur a earum quibusdam.</p>
+                                <Button className='banner-button'>Contato <ArrowRightCircle /></Button>
+                            </div>
+
+                            {/* <TrackVisibility>
+                                {
+                                    ({ isVisible }) => {
+                                        <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                                            <img src={headerImg} alt="Profile Picture" />
+                                        </div>
+                                    }
+                                }
+                            </TrackVisibility> */}
+
                         </Col>
                     </Row>
                 </Container>
             </section>
         </>
     )
-    // const [loopNum, setLoopNum] = useState(0);
-    // const [isDeleting, setIsDeleting] = useState(false);
-    // const [text, setText] = useState('');
-    // const [delta, setDelta] = useState(300 - Math.random() * 100);
-    // const [index, setIndex] = useState(1);
-    // const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
-    // const period = 2000;
-
-    // useEffect(() => {
-    //     let ticker = setInterval(() => {
-    //         tick();
-    //     }, delta);
-
-    //     return () => { clearInterval(ticker) };
-    // }, [text])
-
-    // const tick = () => {
-    //     let i = loopNum % toRotate.length;
-    //     let fullText = toRotate[i];
-    //     let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    //     setText(updatedText);
-
-    //     if (isDeleting) {
-    //         setDelta(prevDelta => prevDelta / 2);
-    //     }
-
-    //     if (!isDeleting && updatedText === fullText) {
-    //         setIsDeleting(true);
-    //         setIndex(prevIndex => prevIndex - 1);
-    //         setDelta(period);
-    //     } else if (isDeleting && updatedText === '') {
-    //         setIsDeleting(false);
-    //         setLoopNum(loopNum + 1);
-    //         setIndex(1);
-    //         setDelta(500);
-    //     }
-    //     else {
-    //         setIndex(prevIndex => prevIndex + 1);
-    //     }
-    // }
-
-    // return (
-    //     <section className="banner" id="home">
-    //         <Container>
-    //             <Row className="aligh-items-center">
-    //                 <Col xs={12} md={6} xl={7}>
-    //                     <TrackVisibility>
-    //                         {({ isVisible }) =>
-    //                             <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-    //                                 <span className="tagline">Welcome to my Portfolio</span>
-    //                                 <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-    //                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-    //                                 <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-    //                             </div>}
-    //                     </TrackVisibility>
-    //                 </Col>
-    //                 <Col xs={12} md={6} xl={5}>
-    //                     <TrackVisibility>
-    //                         {({ isVisible }) =>
-    //                             <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-    //                                 <img src={headerImg} alt="Header Img" />
-    //                             </div>}
-    //                     </TrackVisibility>
-    //                 </Col>
-    //             </Row>
-    //         </Container>
-    //     </section>
-    // )
-
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,6 +6,7 @@ import './navbar.css'
 export default function NavBar({ handleChange, isDark }) {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
+    const [isHidden, setHidden] = useState(true);
 
     useEffect(() => {
         const onScroll = () => {
@@ -24,13 +25,26 @@ export default function NavBar({ handleChange, isDark }) {
 
     const onUpdateActiveLink = (value) => setActiveLink(value);
 
+    // Navbar Collapse on Link Click
+    const navbarMenu = document.querySelector('#basic-navbar-nav');
+    function onLinkClick() {
+        navbarMenu.classList.toggle('show');
+    }
+    const navlinks = document.querySelectorAll('a.nav-link');
+    navlinks.forEach((e) => {
+        e.addEventListener('click', onLinkClick);
+    })
+
+
     return (
+
         <Navbar className={scrolled ? "scrolled" : ""} expand="lg" variant={isDark ? 'dark' : 'light'}>
             <Container>
                 <Navbar.Brand href="#home" className="">OsmarNeto.dev</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setHidden(!isHidden)} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav>
+                        <div id='navbar-blur' className={isHidden ? 'hidden' : ''}></div>
                         <div id='navbar-sections'>
                             <Nav.Link href="#home" className={activeLink === 'home' ? 'active-link navbar-link' : 'navbar-link'} onClick={() => { onUpdateActiveLink('home') }}>
                                 Início
